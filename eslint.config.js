60174a5b5b7231907149fd75dcdd9a8f2fce65ca
iuritorres <iuri.t1000@gmail.com>
2024-04-30 14:49:51 -0300

    chore: add eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,12 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import pluginJs from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+	{
+		languageOptions: { globals: globals.browser },
+		rules: { 'no-unused-vars': 'warn', 'no-undef': 'warn' },
+	},
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
 ];
